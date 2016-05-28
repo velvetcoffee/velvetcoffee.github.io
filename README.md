885d0e3f0f99541e06e2292b7815c5fa9ab41031
chrisdugne <chris.dugne@uralys.com>
2016-05-28 23:34:35 +0200

    new gallery + player + animations

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ```
 Velvet Coffee
 ```
+
+![VelvetCoffee](https://raw.githubusercontent.com/velvetcoffee/velvetcoffee.github.io/master/assets/img/header.jpg)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,44 @@
-jQuery(document).ready(function($){
-	var timelineBlocks = $('.cd-timeline-block'),
-		offset = 0.8;
+function videoTemplate(options){
+	return `
+		<p class="mediaTitle">${options.title}</p>
+		<iframe src="${options.url}"
+		      width="100%"
+		      height="315"
+		      frameborder="0"
+		      webkitallowfullscreen
+		      mozallowfullscreen
+		      allowfullscreen>
+		</iframe>`;
+}
 
-	//hide timeline blocks which are outside the viewport
-	hideBlocks(timelineBlocks, offset);
+function soundcloudTemplate(options){
+	return `
+		<p class="mediaTitle">${options.title}</p>
+		<iframe width="100%"
+		      height="300"
+		      scrolling="no"
+		      frameborder="no"
+		      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${options.id}&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true">
+		</iframe>`;
+}
 
-	//on scolling, show/animate timeline blocks when enter the viewport
-	$(window).on('scroll', function(){
-		(!window.requestAnimationFrame)
-			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
-			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+function showPlayer(options) {
+	var content = {
+		video: videoTemplate,
+		soundcloud: soundcloudTemplate
+	}
+
+	var media = content[options.type](options);
+
+	$('.player').addClass('open');
+	$('.player').addClass('loading');
+	$('.player .media').html(media);
+
+	$('.player .media iframe').load(function () {
+		$('.player').removeClass('loading');
 	});
+}
 
-	function hideBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
-		});
-	}
-
-	function showBlocks(blocks, offset) {
-		blocks.each(function(){
-			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-		});
-	}
-
-});
+function closePlayer() {
+	$('.player').removeClass('open');
+}
